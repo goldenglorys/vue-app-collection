da0115ff091645a3dba5425a5b71b92f47801c5b
goldenglorys <olusolagloryolamide@gmail.com>
2021-02-04 00:09:28 +0100

    Getting the current year, month, and days presnt in the month for display

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="m-auto">
     <h1 class="text-2xl my-2 text-center">Calendly</h1>
+    <section class="mx-2 flex justify-between">
+      <h2 class="font-bold">{{ currentMonthName }}</h2>
+      <h2 class="font-bold">{{ currentYear }}</h2>
+    </section>
     <section class="flex my-2">
       <p
         class="text-center"
@@ -12,7 +16,12 @@
       </p>
     </section>
     <section class="flex flex-wrap">
-      <p class="text-center" style="width: 14.28%" v-for="num in 30" :key="num">
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="num in daysInCurrentMonth(currentYear, currentMonthValue)"
+        :key="num"
+      >
         {{ num }}
       </p>
     </section>
@@ -23,8 +32,17 @@
 export default {
   data() {
     return {
+      currentMonthName: new Date().toLocaleString("default", { month: "long" }),
+      currentMonthValue: new Date().getMonth() + 1,
+      currentYear: new Date().getFullYear(),
       days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     };
+  },
+
+  methods: {
+    daysInCurrentMonth(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
   },
 };
 </script>
